35191ae341550d61211e1114d2fc374ef7537a03
raxxuy <milanilkov@gmail.com>
2026-06-28 23:16:18 +0200

    fix: fix pseudo priorities

--- flake.nix
+++ flake.nix
@@ -4,13 +4,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = (import (inputs.nixpkgs) { inherit system; });
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
-          buildInputs=[
+          buildInputs = [
             pkgs.bun
           ];
         };
